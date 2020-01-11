62671b9ad70f35f626b676698af9c918a3b3e43c
jdvcDev <jdvcDev@donotreply.com>
2020-01-10 21:18:42 -0700

    Phase 7 refinement

--- solver_core/jbrik_solver_phase7.py
+++ solver_core/jbrik_solver_phase7.py
@@ -7,61 +7,54 @@ import jbrik_solver_move_lib
 def solve_oppface_corners(cube):
     log_utils.log("Solving opposite face corners.")
     facetosolve = 3
+    ccolor = cube.get_center_color_for_facenum(facetosolve)
+
+    unsolvedcornerlist = []
+    for cornerrowcell in jbrik_cube.get_cornercell_rowcells_for_face(facetosolve):
+        if not is_corner_solved(cornerrowcell, ccolor, cube):
+            unsolvedcornerlist.append(cornerrowcell)
+
+    if unsolvedcornerlist.__len__() != 0:
+        unsolvedcornerlist.sort()
+        if unsolvedcornerlist.__len__() == 3:
+            unsolvedcorners = unsolvedcornerlist[0] + " " + unsolvedcornerlist[1] + " " + unsolvedcornerlist[2]
+        elif unsolvedcornerlist.__len__() == 2:
+            unsolvedcorners = unsolvedcornerlist[0] + " " + unsolvedcornerlist[1]
+
+        # orient so that (top) oppface front right corner is unsolved (as well as the front left corner) if you can get both
+        # find right hand face for solved rowcell combo
+        tface = facetosolve.__str__()
+        bface = "1"
+        rface = jbrik_cube.oppfacecell_rface_align_map[unsolvedcorners].split(" ")[0].__str__()
+        cornertosolve = jbrik_cube.oppfacecell_rface_align_map[unsolvedcorners].split(" ")[1]
+
+        # special case where solved rows are opposites
+        if unsolvedcorners == "7.1 9.3" or unsolvedcorners == "7.3 9.1":
+            # rotate once to move unsolved corner into top/front/right
+            cube = jbrik_solver_move_lib.perform_rotation_str(tface + "CW1", cube)
+
+        # do this algo 2, check for solve
+        # if not solved, 2 more times
+        # rotate top until next unsolved corner is in front top right
+        # (R' D' R D) x2/4 + u inbetween and after
+        movelist = [rface + "CC1", bface + "CC1", rface + "CW1", bface + "CW1"]
+
+        for i in range(0, unsolvedcornerlist.__len__()):
+            # after 2 check for solve,
+            attemptcount = 0
+            while not is_corner_solved(cornertosolve, ccolor, cube) and attemptcount < 2:
+                cube = attempt_corner_solve(movelist, cube)
+                attemptcount += 1
 
 
+            # rotate until next unsolved is in top/front/right, never more than 2 turns
+            attemptcount = 0
+            while is_corner_solved(cornertosolve, ccolor, cube) and attemptcount < 2:
+                cube = jbrik_solver_move_lib.perform_rotation_str(tface + "CW2", cube)
+                attemptcount += 1
 
-    #for 7.1
-#    solverowcell = "7.1"
-#    facemap = jbrik_cube.oppface_cell_face_map[solverowcell]
-#    tface = facetosolve.__str__()
-#    bface = "1"
-#    rface = facemap[3].__str__() #6
-
-
-    # identify the corners that are not ccolor(unsolvedsolved)
-    unsolvedrowcells = "7.3 9.1"
-
-    # orient so that (top) oppface front right corner is unsolved (as well as the front left corner) if you can get both
-    # find right hand face for solved rowcell combo
-    tface = facetosolve.__str__()
-    bface = "1"
-    rface = jbrik_cube.oppfacecell_rface_align_map[unsolvedrowcells].__str__()
-
-    # special case where solved rows are opposites
-    if unsolvedrowcells == "7.1 9.3" or unsolvedrowcells == "7.3 9.1":
-        # rotate once to move unsolved corner into top/front/right
+        # final rotation
         cube = jbrik_solver_move_lib.perform_rotation_str(tface + "CW1", cube)
-
-    # do this algo 2, check for solve
-    # if not solved, 2 more times
-    # rotate top until next unsolved corner is in front top right
-    # (R' D' R D) x2/4 + u inbetween and after
-    movelist = [rface + "CC1", bface + "CC1", rface + "CW1", bface + "CW1"]
-
-
-    # do to rotations, if not solved in two do two more, pretend takes two for use case
-#    for i in range(1, 3):
-#        for rmove in movelist:
-#            cube = jbrik_solver_move_lib.perform_rotation_str(rmove, cube)
-
-    cube = attempt_corner_solve(movelist, cube)
-
-    # after 2 check for solve, pretend it is
-    # simulate opposite corners solving 7.1
-
-    # rotate until next unsolved is in top/front/right
-    cube = jbrik_solver_move_lib.perform_rotation_str(tface + "CW2", cube)
-
-    # do to rotations, if not solved in two do two more, pretend takes four for use case
-#    for i in range(1, 5):
-#        for rmove in movelist:
-#            cube = jbrik_solver_move_lib.perform_rotation_str(rmove, cube)
-
-    cube = attempt_corner_solve(movelist, cube)
-    cube = attempt_corner_solve(movelist, cube)
-
-    cube = jbrik_solver_move_lib.perform_rotation_str(tface + "CW1", cube)
-
 
     cube.finalize_solve_phase()
     log_utils.log("Opposite face corners solved.")
@@ -73,3 +66,10 @@ def attempt_corner_solve(movelist, cube):
             cube = jbrik_solver_move_lib.perform_rotation_str(rmove, cube)
 
     return cube
+
+def is_corner_solved(rowcell, ccolor, cube):
+    if cube.get_cell_val_by_rowcell(rowcell) == ccolor:
+        return True
+
+    return False
+
